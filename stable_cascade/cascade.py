4db1016e309fb8bb4e4a49e7bf2a87043ec45df0
Zoya <134557773+zoyahammad@users.noreply.github.com>
2024-05-23 00:29:54 +0200

    added cascade files

---
+++ stable_cascade/cascade.py
@@ -0,0 +1,24 @@
+import torch
+import time
+from diffusers import StableCascadeCombinedPipeline
+
+start_time = time.time()
+
+pipe = StableCascadeCombinedPipeline.from_pretrained("stabilityai/stable-cascade", variant="bf16", torch_dtype=torch.float32)
+device = torch.device('mps')
+pipe = pipe.to(device)
+
+PROMPT = "an astronaut"
+
+for i in range(0, 1):
+    pipe(
+        prompt=PROMPT,
+        negative_prompt="",
+        num_inference_steps=10,
+        prior_num_inference_steps=20,
+        prior_guidance_scale=3.0,
+        width=1024,
+        height=1024,
+    ).images[0].save(f'cascade/doctor/cascade-{i}.png')
+
+print(" Total time --- %s seconds ---" % (time.time() - start_time))
